23854074dda475ba1988463bbbb97e58b5668f46
cybeloras <cybeloras@14367993-9c92-4b4e-ace5-e8357f8c5e4c>
2013-03-27 09:09:59 +0000

    Updated to work with the 50 stable slots that are available in patch 5.3
    git-svn-id: https://repos.curseforge.com/wow/improved-stable-frame/trunk@16 14367993-9c92-4b4e-ace5-e8357f8c5e4c

--- ImprovedStableFrame.lua
+++ ImprovedStableFrame.lua
@@ -1,44 +1,56 @@
 ﻿
--- --------------------------
--- Improved Stable Frame
--- By Cybeloras of Detheroc/Mal'Ganis
--- --------------------------
+local clientVersion = select(4, GetBuildInfo())
+local wow_503 = clientVersion >= 50300
 
-for i = 11, 20 do 
-    if not _G["PetStableStabledPet"..i] then
-        CreateFrame("Button", "PetStableStabledPet"..i, PetStableFrame, "PetStableSlotTemplate", i)
-    end
+local maxSlots = NUM_PET_STABLE_PAGES * NUM_PET_STABLE_SLOTS
+
+local NUM_PER_ROW, heightChange
+if wow_503 then
+	NUM_PER_ROW = 10
+	heightChange = 36
+else
+	NUM_PER_ROW = 7
+	heightChange = 17
 end
 
-for i = 1, 20 do
+for i = NUM_PET_STABLE_SLOTS + 1, maxSlots do 
+	if not _G["PetStableStabledPet"..i] then
+		CreateFrame("Button", "PetStableStabledPet"..i, PetStableFrame, "PetStableSlotTemplate", i)
+	end
+end
+
+for i = 1, maxSlots do
 	local frame = _G["PetStableStabledPet"..i]
 	if i > 1 then
 		frame:ClearAllPoints()
-		frame:SetPoint("LEFT", _G["PetStableStabledPet"..i-1], "RIGHT", 7, 0)
+		frame:SetPoint("LEFT", _G["PetStableStabledPet"..i-1], "RIGHT", 7.3, 0)
 	end
 	frame:SetFrameLevel(PetStableFrame:GetFrameLevel() + 1)
+	frame:SetScale(7/NUM_PER_ROW)
 end
 
 PetStableStabledPet1:ClearAllPoints()
-PetStableStabledPet1:SetPoint("TOPLEFT", PetStableBottomInset, 10, -9)
+PetStableStabledPet1:SetPoint("TOPLEFT", PetStableBottomInset, 9, -9)
 
-PetStableStabledPet8:ClearAllPoints()
-PetStableStabledPet8:SetPoint("TOPLEFT", PetStableStabledPet1, "BOTTOMLEFT", 0, -5)
-
-PetStableStabledPet15:ClearAllPoints()
-PetStableStabledPet15:SetPoint("TOPLEFT", PetStableStabledPet8, "BOTTOMLEFT", 0, -5)
+for i = NUM_PER_ROW+1, maxSlots, NUM_PER_ROW do
+	_G["PetStableStabledPet"..i]:ClearAllPoints()
+	_G["PetStableStabledPet"..i]:SetPoint("TOPLEFT", _G["PetStableStabledPet"..i-NUM_PER_ROW], "BOTTOMLEFT", 0, -5)
+end
 
 PetStableNextPageButton:Hide()
 PetStablePrevPageButton:Hide()
 
-PetStableFrameModelBg:SetHeight(265)
-PetStableFrameModelBg:SetTexCoord(0.16406250, 0.77734375, 0.00195313, 0.51942003)
 
-PetStableFrameInset:SetPoint("BOTTOMRIGHT", PetStableFrame, "BOTTOMRIGHT", -6, 142)
+PetStableFrameModelBg:SetHeight(281 - heightChange)
+PetStableFrameModelBg:SetTexCoord(0.16406250, 0.77734375, 0.00195313, 0.55078125 - heightChange/512)
 
-PetStableFrameStableBg:SetHeight(134)
+PetStableFrameInset:SetPoint("BOTTOMRIGHT", PetStableFrame, "BOTTOMRIGHT", -6, 126 + heightChange)
 
-NUM_PET_STABLE_SLOTS = 20
+PetStableFrameStableBg:SetHeight(116 + heightChange)
+
+
+
+NUM_PET_STABLE_SLOTS = maxSlots
 NUM_PET_STABLE_PAGES = 1
 PetStableFrame.page = 1
 
